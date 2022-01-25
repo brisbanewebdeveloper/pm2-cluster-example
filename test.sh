e93010b087cb6c78ea06e25ba2a067cb759f0dcb
Brisbane Web Developer <brisbanewebdeveloper@outlook.com>
2022-01-25 20:36:02 +1000

    Amended the test to finish faster

--- test.sh
+++ test.sh
@@ -3,13 +3,19 @@
 node index.js --verbose
 
 echo
-node index.js -i 2 --verbose
+# node index.js -i 2 --verbose
+node index.js -i 2
 
 echo
-node index.js -i 3 --verbose
+# node index.js -i 3 --verbose
+node index.js -i 3
 
 echo
 node index.js -i 4
 
 echo
 node index.js -i 7
+
+# Gets the same result with 7
+# echo
+# node index.js -i 12
